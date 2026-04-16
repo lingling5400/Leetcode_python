99e514642183ac8682fee9a462b81a49b1b728a5
lingling5400 <136593138+lingling5400@users.noreply.github.com>
2026-04-16 16:42:39 +0800

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -18,3 +18,46 @@ class Solution(object):
                 seen.add(s[right])
                 ans=max(ans,right-left+1)
         return ans
+
+
+
+# ===== Execution Flow =====
+# s = "sfjeif"
+
+# right=0 => seen{} 
+# → s not in seen → {s} → ans=1
+
+# right=1 => {s}
+# → f not in seen → {s,f} → ans=2
+
+# right=2 => {s,f}
+# → j not in seen → {s,f,j} → ans=3
+
+# right=3 => {s,f,j}
+# → e not in seen → {s,f,j,e} → ans=4
+
+# right=4 => {s,f,j,e}
+# → i not in seen → {s,f,j,e,i} → ans=5
+
+# ----------------------------------
+
+# right=5 => {s,f,j,e,i}
+# → f in seen ❗
+
+# while 開始
+
+# left=0 → remove 's'
+# → seen = {f,j,e,i}, left=1
+
+# left=1 → remove 'f'
+# → seen = {j,e,i}, left=2
+
+# while 結束（f 已不在）
+
+----------------------------------
+
+# right=5, left=2 => {j,e,i}
+# → f not in seen → add f
+# → seen = {j,e,i,f}
+
+# ans = 5
